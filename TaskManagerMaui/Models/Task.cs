0ef0893fe9b032f7adbc4610f73284d34b1d6da4
JordyMorenoArias <yordimorenoarias.11@gmail.com>
2025-02-22 16:55:55 -0400

    It was created the class Task with their properties together the enum Priority

--- TaskManagerMaui/Models/Task.cs
+++ TaskManagerMaui/Models/Task.cs
@@ -1,13 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskManagerMaui.Constants;
 
 namespace TaskManagerMaui.Models
 {
     class Task
     {
-
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        [Required]
+        public Priority Priority { get; set; }
+        [Required]
+        public DateTime DueDate { get; set; }
+        public bool IsCompleted { get; set; } = false;
     }
 }
