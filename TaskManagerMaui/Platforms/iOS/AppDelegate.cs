969d00c11fe39819252b5911257333896d068725
JordyMorenoArias <yordimorenoarias.11@gmail.com>
2025-03-03 18:00:12 -0400

    Refactor: Removed TaskManagerMaui project to separate the API Chore: Removed TaskManagerMaui and renamed the solution to TaskManagerAPI

--- TaskManagerMaui/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace TaskManagerMaui;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
